beaae0c7ab668a1cb13e147dea8b99c65185f72d
SomeDudeChilling <jonathonacosta591@riversideunified.org>
2025-02-19 09:15:26 -0800

    wef

--- Schizo EdgeMaxxxers/Assets/Scripts/Footsteps.cs
+++ Schizo EdgeMaxxxers/Assets/Scripts/Footsteps.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Footsteps : MonoBehaviour
 {
-    public audiosource footstepsSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        
     }
 }
