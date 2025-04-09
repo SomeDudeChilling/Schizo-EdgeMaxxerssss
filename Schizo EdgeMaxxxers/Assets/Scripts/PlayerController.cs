7c0e1da60d90dc58ac31b84540a4d1d7f8a0a417
SomeDudeChilling <jonathonacosta591@riversideunified.org>
2025-04-09 09:16:01 -0700

    qdfew

--- Schizo EdgeMaxxxers/Assets/Scripts/PlayerController.cs
+++ Schizo EdgeMaxxxers/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject bullet;
+    public Transform firePoint;
+    public Transform theCamera;
+    private Ammo _ammo;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _ammo = GetComponent<Ammo>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Handle Shooting
+        if(Input.GetMouseButtonDown(0) && _ammo.GetAmmoAmount() > 0)
+        {
+            //Find the crosshair
+            RaycastHit hit;
+            if(Physics.Raycast(theCamera.position, theCamera.forward, out hit, 50f))
+            {
+                if(Vector3.Distance(theCamera.position, hit.point) > 2f)
+                {
+                    firePoint.LookAt(hit.point);
+                }
+            }
+            else
+            {
+                firePoint.LookAt(theCamera.position + (theCamera.forward * 30f));
+            }
+
+            //Create the bullet
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+
+            //Remove ammo
+            _ammo.RemoveAmmo();
+        }
     }
 }
